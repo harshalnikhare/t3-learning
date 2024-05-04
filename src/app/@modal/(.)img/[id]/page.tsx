03a8968960c341af7dcbfae48ada50440ab5fb79
Harshal Nikhare <harshaln369@gmail.com>
2024-05-04 18:14:55 +0530

    Start implementing parallel routes

---
+++ src/app/@modal/(.)img/[id]/page.tsx
@@ -0,0 +1,19 @@
+import { getImage } from "~/server/queries";
+
+export default async function PhotoModal({
+  params: { id: photoId },
+}: {
+  params: { id: string };
+}) {
+  const idAsNumber = Number(photoId);
+  console.log("idAsNumber", idAsNumber);
+
+  if (Number.isNaN(idAsNumber)) throw new Error("Invalid photo id");
+
+  const image = await getImage(idAsNumber);
+  return (
+    <div>
+      <img src={image.url} className="w-96" />
+    </div>
+  );
+}
